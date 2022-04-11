6c6be8528de7fc02bb1d1cb8e8cea4dea1b6da99
kevinguvenal <guvenalkevin@gmail.com>
2022-04-11 14:18:55 +0200

    lab1 test

--- Lab_2/src/main.cpp
+++ Lab_2/src/main.cpp
@@ -1,12 +1,54 @@
 #include <Arduino.h>
+#include <math.h>
 
-void setup()
-{
+int counter = 0;
+int buttonState = 0;
+
+void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  pinMode(11, OUTPUT);  
+  pinMode(12, INPUT_PULLUP);
 }
 
-void loop()
-{
-  // put your main code here, to run repeatedly:
+void loop() {
+
+  buttonState = digitalRead(12);
+
+  buttonState = digitalRead(12);
+ 
+  
+  if (buttonState == LOW) // light the LED
+  {
+    counter++;
+    delay(150);
+    Serial.println("Pressed");
+  }
+
+  if (counter == 0)
+  {
+    digitalWrite(11, LOW);
+  }
+  
+  else if (counter == 1)
+  {
+    digitalWrite(11, HIGH);
+  }
+
+  else if (counter == 2)
+  {
+    digitalWrite(11, LOW);
+  }
+
+  else if (counter == 3)
+  {
+    digitalWrite(11, LOW);
+  }
+
+  else
+  {
+    counter = 0;
+  }
+
+   Serial.println(counter);
 }
