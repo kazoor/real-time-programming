cde4ce63f5a826596d0b015999ba7c1a47e0ab97
andreedetun <andre.edetun.falk@gmail.com>
2022-04-29 13:21:51 +0200

    Rewriting B2

--- Lab_2/src/main.cpp
+++ Lab_2/src/main.cpp
@@ -2,6 +2,22 @@
 #include <Arduino_FreeRTOS.h>
 #include <semphr.h>
 #define pdMS_TO_TICKS(xTimeInMs) ((TickType_t)(((unsigned long)(xTimeInMs) * (TickType_t)configTICK_RATE_HZ) / (TickType_t)1000))
+#define MAX_LED_ALPHA 255
+/*
+    3 tasks with period set to 100ms and priorities and task 3 < task 2
+    t1 & t2 will control LED's using the PMW (lab1).
+    t3 will read command frmo serial port. serial sent to 1 or 2 based on cmd
+    each of the global vars are accesed by 2 taks and MUST be protected using synchronization
+    clear it (set to -1) if there is no command meaning -1 is should ignore cmd
+    program can only hold 1 cmd.
+
+    n.m
+    n = task number (t1, t2)
+    m = brightnes of LED controlled by task n
+    example: if command 1.25 = shining 25%.
+    depending on n, t3 delivers cmd to t1 or t2. LED continues with same state
+    until it is changed.
+*/
 
 struct ledParams
 {
@@ -10,7 +26,9 @@ struct ledParams
 };
 
 char incomingByte;
-int answer = 0;
+float inputValue = 0;
+int incomingCommand = 0;
+
 TaskHandle_t ledTask1Handle;
 TaskHandle_t ledTask2Handle;
 
@@ -43,30 +61,28 @@ void inputTask(void *param)
         if (Serial.available() > 0)
         {
             Serial.print("Input: ");
-            incomingByte = Serial.read();
-            answer = incomingByte - '0';
-            Serial.println(answer);
-            if (answer == 1)
-            {
-                xTaskCreate(ledTask1, "LedTask1", 128, (void *)leds, 1, &ledTask1Handle);
-            }
-            else if (answer == 2)
-            {
-                xTaskCreate(ledTask2, "LedTask2", 128, (void *)leds, 2, &ledTask2Handle);
-            }
-            else if (answer < 0)
-            {
-                analogWrite(10, 0);
-                analogWrite(11, 0);
-                if (ledTask1Handle)
-                {
-                    vTaskDelete(ledTask1Handle);
-                }
-                else if (ledTask2Handle)
-                {
-                    vTaskDelete(ledTask2Handle);
-                }
-            }
+            inputValue = Serial.parseFloat();
+            Serial.println(inputValue);
+
+            int whole = (int)inputValue;
+            Serial.print("Whole:  ");
+            Serial.print(whole);
+            Serial.print("\n");
+
+            float decimal = inputValue - whole;
+            Serial.print("Decimal: ");
+            Serial.print(decimal);
+            Serial.print("\n");
+
+            int calculatedAlpha = MAX_LED_ALPHA * decimal;
+            Serial.println(calculatedAlpha);
+
+            Serial.println("----------------------------");
+
+            // 0.25
+            // Max == 255
+            // -> 1.25
+            // led = 1, alpha = max * 0.25
         }
         vTaskDelay(pdMS_TO_TICKS(100));
     }
