ad0927ccd9977b24531de755164dbaf4630eed33
kevinguvenal <guvenalkevin@gmail.com>
2022-10-17 20:18:15 +0200

    finishing lab5 and adding comments

--- Lab_5/main.cpp
+++ Lab_5/main.cpp
@@ -5,14 +5,7 @@
 #include <stdlib.h>
 #include <IRremote.h>
 
-// Distande sensor
-// IR remote
-
-/*
-IRrecv receiver(RECV_PIN); Where RECV_PIN is the digital pin number on Arduino.
-
-Then start the reciever: receiver.enableIRIn();
-*/
+// IR remote defines
 #define POWER 0x00FF629D
 #define A 0x00FF22DD
 #define B 0x00FF02FD
@@ -23,21 +16,41 @@ Then start the reciever: receiver.enableIRIn();
 #define RIGHT 0x00FF7A85
 #define SELECT 0x00FF18E7
 
+// IR sensor pin
+#define Signal_PIN 3 // green
+
 // distance sensor HC-SR04 HY-SRF05
 #define TRIG_PIN 13 // red
 #define ECHO_PIN 12 // orange
 
-// IR sensor
-#define Signal_PIN 11 // green
+// Left motor direction pins
+#define IN1_PIN 9
+#define IN2_PIN 8
 
+// Right motor direction pins
+#define IN3_PIN 4
+#define IN4_PIN 7
+
+// Motor PWM pins
+#define MOTOR_LEFT_PIN 6
+#define MOTOR_RIGHT_PIN 5
+
+#define MAX_SPEED 255.0f
+
+SemaphoreHandle_t mutex;
 long duration = 0, cm = 0;
+static float speed = 0.0f;
 IRrecv irrecv(Signal_PIN);
 decode_results results;
+static bool forward = false;
 
+// handles the distance sensor HC-SR04 HY-SRF05, if the distance is less or equal to 20 the engines will stop
+// and will also block the forward movement until the distance is less than 20.
 void distanceSensorTask(void *param)
 {
     for (;;)
     {
+        // code for the sensor to even work.
         long distance;
         digitalWrite(TRIG_PIN, LOW);
         delayMicroseconds(5);
@@ -50,15 +63,30 @@ void distanceSensorTask(void *param)
 
         cm = (duration / 2) / 29.1;
 
-        if (cm <= 10)
+        // check distance, if its less or equal than 20 block forward movement and stop engines.
+        // also take the semaphore to make sure nothing else changes the values.
+        xSemaphoreTake(mutex, portMAX_DELAY);
+        if (cm <= 20 && forward)
         {
-            Serial.println(cm);
+            speed = 0.0f;
+            digitalWrite(IN1_PIN, LOW);
+            digitalWrite(IN2_PIN, LOW);
+            digitalWrite(IN3_PIN, LOW);
+            digitalWrite(IN4_PIN, LOW);
+
+            analogWrite(MOTOR_LEFT_PIN, 0);
+            analogWrite(MOTOR_RIGHT_PIN, 0);
+            Serial.println("DISTANCE CRITICAL.");
         }
+
+        // give back semaphore
+        xSemaphoreGive(mutex);
 
         vTaskDelay(pdMS_TO_TICKS(100));
     }
 }
 
+// this task handles all the IR input and if an input is received send it to the translateIR function that handles the input.
 void irSensorTask(void *param)
 {
     for (;;)
@@ -80,7 +108,15 @@ void setup()
     pinMode(TRIG_PIN, OUTPUT);
     pinMode(ECHO_PIN, INPUT);
 
-    irrecv.enableIRIn();
+    pinMode(IN1_PIN, OUTPUT);
+    pinMode(IN2_PIN, OUTPUT);
+    pinMode(IN3_PIN, OUTPUT);
+    pinMode(IN4_PIN, OUTPUT);
+    pinMode(MOTOR_LEFT_PIN, OUTPUT);
+    pinMode(MOTOR_RIGHT_PIN, OUTPUT);
+
+    // create mutex
+    mutex = xSemaphoreCreateMutex();
 
     xTaskCreate(distanceSensorTask, "Distance_sensor", 100, nullptr, 2, NULL);
     xTaskCreate(irSensorTask, "IR_sensor", 100, nullptr, 1, NULL);
@@ -88,39 +124,84 @@ void setup()
 
 void loop()
 {
+    
 }
 
+// This controls the speed of the car's wheels based on the user input with the controller
 void translateIR()
 {
-    switch (results.value)
+    xSemaphoreTake(mutex, portMAX_DELAY); // take semaphore
+    
+    //stop engines when power button is pressed.
+    if(results.value == POWER)
     {
-    case POWER:
-        Serial.println("POWER");
-        break;
+        digitalWrite(IN1_PIN, LOW);
+        digitalWrite(IN2_PIN, LOW);
+        digitalWrite(IN3_PIN, LOW);
+        digitalWrite(IN4_PIN, LOW);
 
-    case UP:
-        Serial.println("UP");
-        break;
-
-    case DOWN:
-        Serial.println("DOWN");
-        break;
-
-    case SELECT:
-        Serial.println("SELECT");
-        break;
-
-    case LEFT:
-        Serial.println("LEFT");
-        break;
-    
-    case RIGHT:
-        Serial.println("RIGHT");
-        break;
-    
-    default:
-        break;
+        analogWrite(MOTOR_LEFT_PIN, 0);
+        analogWrite(MOTOR_RIGHT_PIN, 0);
     }
+    else if(results.value == UP) // forward movement
+    {
+        digitalWrite(IN1_PIN, HIGH);
+        digitalWrite(IN2_PIN, LOW);
+        digitalWrite(IN3_PIN, LOW);
+        digitalWrite(IN4_PIN, HIGH);
 
+        analogWrite(MOTOR_LEFT_PIN, speed);
+        analogWrite(MOTOR_RIGHT_PIN, speed);
+        Serial.println("UP");
+        forward = true;
+    }
+    else if(results.value == DOWN) // backward movement
+    {        
+        digitalWrite(IN1_PIN, LOW);
+        digitalWrite(IN2_PIN, HIGH);
+        digitalWrite(IN3_PIN, HIGH);
+        digitalWrite(IN4_PIN, LOW);
+
+        analogWrite(MOTOR_LEFT_PIN, speed);
+        analogWrite(MOTOR_RIGHT_PIN, speed);
+        forward = false;
+    }
+    else if(results.value == RIGHT) // right movement
+    {
+        digitalWrite(IN1_PIN, LOW);
+        digitalWrite(IN2_PIN, HIGH);
+        digitalWrite(IN3_PIN, LOW);
+        digitalWrite(IN4_PIN, HIGH);
+
+        analogWrite(MOTOR_LEFT_PIN, speed);
+        analogWrite(MOTOR_RIGHT_PIN, speed);
+        forward = false;
+    }
+    else if(results.value == LEFT) // left movement
+    {
+        digitalWrite(IN1_PIN, HIGH);
+        digitalWrite(IN2_PIN, LOW);
+        digitalWrite(IN3_PIN, HIGH);
+        digitalWrite(IN4_PIN, LOW);
+
+        analogWrite(MOTOR_LEFT_PIN, speed);
+        analogWrite(MOTOR_RIGHT_PIN, speed);
+        forward = false;
+    }
+    else if(results.value == A) // set speed to 50%
+    {
+        speed = MAX_SPEED / 2.0;
+    }
+    else if(results.value == B) // set speed to 75%
+    {
+        speed = MAX_SPEED * 0.75f;
+    }
+    else if(results.value == C) // set max speed (255)
+    {
+        speed = MAX_SPEED;
+    }
+   
+    xSemaphoreGive(mutex); // give back semaphore
+    
     delay(100); // to not get immediate repeat
 }
